4384e0e8b184d68b42a16aff50ee380a00e33d50
Wesley Paternezi <wesley.paternezi@bandtec.com.br>
2021-11-06 18:06:16 -0300

    progressBar animation

--- wasd-prototipo-java/src/main/java/br/com/wasd/wasd/prototipo/java/LoadingScreen.java
+++ wasd-prototipo-java/src/main/java/br/com/wasd/wasd/prototipo/java/LoadingScreen.java
@@ -1,9 +1,26 @@
-
 package br.com.wasd.wasd.prototipo.java;
+
+import java.awt.Color;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class LoadingScreen extends javax.swing.JFrame {
 
     public LoadingScreen() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LoadingScreen.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(LoadingScreen.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(LoadingScreen.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(LoadingScreen.class.getName()).log(Level.SEVERE, null, ex);
+        }
         initComponents();
     }
 
@@ -14,14 +31,14 @@ public class LoadingScreen extends javax.swing.JFrame {
         BackgroundColor = new javax.swing.JPanel();
         LoadingLabel = new javax.swing.JLabel();
         LoadingValue = new javax.swing.JLabel();
-        jProgressBar1 = new javax.swing.JProgressBar();
+        LoadingBar = new javax.swing.JProgressBar();
         gif_wasd = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(0, 0, 0));
 
         BackgroundColor.setBackground(new java.awt.Color(0, 0, 0));
-        BackgroundColor.setForeground(new java.awt.Color(149, 173, 253));
+        BackgroundColor.setForeground(new java.awt.Color(0, 0, 0));
         BackgroundColor.setPreferredSize(new java.awt.Dimension(705, 532));
 
         LoadingLabel.setBackground(new java.awt.Color(255, 255, 255));
@@ -34,8 +51,11 @@ public class LoadingScreen extends javax.swing.JFrame {
         LoadingValue.setForeground(new java.awt.Color(255, 255, 255));
         LoadingValue.setText("0 %");
 
-        jProgressBar1.setForeground(new java.awt.Color(149, 173, 253));
+        LoadingBar.setBackground(new java.awt.Color(0, 0, 0));
+        LoadingBar.setFont(new java.awt.Font("Yu Gothic UI Semilight", 0, 3)); // NOI18N
+        LoadingBar.setForeground(new java.awt.Color(0, 0, 0));
 
+        gif_wasd.setBackground(new java.awt.Color(0, 0, 0));
         gif_wasd.setIcon(new javax.swing.JLabel() {
             public javax.swing.Icon getIcon() {
                 try {
@@ -53,28 +73,25 @@ public class LoadingScreen extends javax.swing.JFrame {
         BackgroundColor.setLayout(BackgroundColorLayout);
         BackgroundColorLayout.setHorizontalGroup(
             BackgroundColorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jProgressBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(LoadingBar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(BackgroundColorLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(BackgroundColorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(gif_wasd, javax.swing.GroupLayout.PREFERRED_SIZE, 680, Short.MAX_VALUE)
-                    .addGroup(BackgroundColorLayout.createSequentialGroup()
-                        .addComponent(LoadingLabel)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(LoadingValue, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addComponent(LoadingLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 553, Short.MAX_VALUE)
+                .addComponent(LoadingValue, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
+            .addComponent(gif_wasd, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
         );
         BackgroundColorLayout.setVerticalGroup(
             BackgroundColorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, BackgroundColorLayout.createSequentialGroup()
-                .addGap(55, 55, 55)
-                .addComponent(gif_wasd, javax.swing.GroupLayout.PREFERRED_SIZE, 328, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(gif_wasd, javax.swing.GroupLayout.PREFERRED_SIZE, 383, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 107, Short.MAX_VALUE)
                 .addGroup(BackgroundColorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(LoadingValue)
                     .addComponent(LoadingLabel))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(LoadingBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -109,18 +126,47 @@ public class LoadingScreen extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(LoadingScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
 
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new LoadingScreen().setVisible(true);
+        LoadingScreen sp = new LoadingScreen();
+        sp.setVisible(true);
+        sp.LoadingBar.setStringPainted(true);
+        ScreenLoading(sp);
+    }
+
+    public static void ScreenLoading(LoadingScreen sp) {
+        try {
+            sp.LoadingBar.setForeground(new Color(149, 173, 253));
+
+            for (int i = 0; i <= 100; i++) {
+                Thread.sleep(100);
+                sp.LoadingValue.setText(i + "%");
+
+                if (i == 10) {
+                    sp.LoadingLabel.setText("Capturando dados do sistema...");
+                }
+                if (i == 30) {
+                    sp.LoadingLabel.setText("Capturando dados do hardware...");
+                }
+                if (i == 50) {
+                    sp.LoadingLabel.setText("Conectando com o Banco de Dados...");
+                }
+                if (i == 70) {
+                    sp.LoadingLabel.setText("Carregando Interface...");
+                }
+                if (i == 85) {
+                    sp.LoadingLabel.setText("Iniciando Aplicação...");
+                }
+                sp.LoadingBar.setValue(i);
             }
-        });
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel BackgroundColor;
+    private javax.swing.JProgressBar LoadingBar;
     private javax.swing.JLabel LoadingLabel;
     private javax.swing.JLabel LoadingValue;
     private javax.swing.JLabel gif_wasd;
-    private javax.swing.JProgressBar jProgressBar1;
     // End of variables declaration//GEN-END:variables
 }
